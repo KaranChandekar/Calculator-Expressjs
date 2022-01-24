d898152343efca0cef783b7c67bd6b7fbb26dfb1
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-24 22:36:06 +0530

    add body-parser

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
