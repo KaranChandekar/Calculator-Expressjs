5a368021a2f71abdcbb462f11093c8038a6e2b7b
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-24 22:21:48 +0530

    res.sendFile

--- calculator.js
+++ calculator.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function () {
