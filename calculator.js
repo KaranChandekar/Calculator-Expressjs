5fe0044e846a1854a427795b10de94b2cb51baf4
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-24 22:29:09 +0530

    add app.post

--- calculator.js
+++ calculator.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function (req, res) {
+  res.send("Thanks for posting that!");
+});
+
 app.listen(3000, function () {
   console.log("Server is running at port 3000.");
 });
