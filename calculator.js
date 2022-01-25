22d77629bc2d54fe90dd51ff104f611a6bb42ddf
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-25 20:42:53 +0530

    app.post bmicalculator

--- calculator.js
+++ calculator.js
@@ -21,6 +21,13 @@ app.get("/bmicalculator", function (req, res) {
   res.sendFile(__dirname + "/bmiCalculator.html");
 });
 
+app.post("/bmicalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height * height);
+  res.send("Your BMI is " + bmi);
+});
+
 app.listen(3000, function () {
   console.log("Server is running at port 3000.");
 });
