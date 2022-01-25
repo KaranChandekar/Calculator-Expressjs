926dceeee54aa968085dd7a150350b1b512fbdc2
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-25 14:53:32 +0530

    res.send result

--- calculator.js
+++ calculator.js
@@ -11,7 +11,10 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  res.send("Thanks for posting that!");
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+  res.send("The result of the calculation is " + result);
 });
 
 app.listen(3000, function () {
